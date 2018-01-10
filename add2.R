107a6adb0ca1ee00261d1cfadb58a90871d20a8e
citation891 <amtrigo891@gmail.com>
2018-01-10 08:56:32 -0500

    added add2.R and modified add.R

---
+++ add2.R
@@ -0,0 +1,9 @@
+add <- function(x,y) x + y    # this is the original function (incorrect)
+
+# Initialize git tracking.  change directory to arithmetic folder 
+# gitbash Cmd: 'C:\Users\amtri\Desktop\AFIT\Final Class\Arithmetic'
+# gitbash Cmd: git init
+# gitbash Cmd: git status
+# gitbash Cmd: git add 'add.R'
+# gitbash Cmd: git status         # shows changes to be comitted
+# gitbash Cmd: git commit -m "added add.R file with multiplication error"
